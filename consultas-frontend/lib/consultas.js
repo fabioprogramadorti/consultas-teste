a53dcbf39b5d31bc4009f44661e4217cdab33075
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-04-16 07:37:46 -0300

    Add configuration for dotenv file

--- consultas-frontend/lib/consultas.js
+++ consultas-frontend/lib/consultas.js
@@ -5,35 +5,36 @@ import axios from 'axios'
 const API_URL = process.env.API_URL
 
 export async function getAllConsultas() {
-  const res = await axios.get('http://localhost:5000/consultas')
+  const res = await axios.get(`${API_URL}/consultas`)
   const consultas = res.data
   return consultas
 }
 
 export async function getConsulta(id) {
-  const res = await axios.get(`http://localhost:5000/consultas/${id}`)
+  const res = await axios.get(`${API_URL}/consultas/${id}`)
   const consulta = res.data
   return consulta
 }
 
 export async function createConsulta(consulta) {
-  const res = await axios.post('http://localhost:5000/consultas', consulta)
+const res = await axios.post(`${API_URL}/consultas`, consulta)
   const novaConsulta = res.data
   return novaConsulta
 }
 
 export async function updateConsulta(id, consulta) {
-  const res = await axios.put(`http://localhost:5000/consultas/${id}`, consulta)
+  const res = await axios.put(`${API_URL}/consultas/${id}`, consulta)
   const consultaModificada = res.data
   return consultaModificada
 }
 
 export async function deleteConsulta(id) {
-  const res = await axios.delete(`http://localhost:5000/consultas/${id}`)
+  const res = await axios.delete(`${API_URL}/consultas/${id}`)
 }
 
 export async function getAllMedicos(id) {
-  const res = await axios.get('http://localhost:5000/medicos')
+  console.log(API_URL)
+  const res = await axios.get(`${API_URL}/medicos`)
   const medicos = res.data
   const nomesMedicos = medicos.map(medico => {return medico.nome})
   return nomesMedicos
